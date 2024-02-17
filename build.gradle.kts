64fe349253d9e49a6726de2476e8b4b3e7ac4eae
skyneton <donginbae30@gmail.com>
2024-02-17 19:20:05 +0900

    block place bug fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,23 +3,28 @@ plugins {
 }
 
 group = "net.mpoisv.locker"
-version = "1.8-SNAPSHOT"
+version = "1.9-SNAPSHOT"
 
 repositories {
     mavenCentral()
     mavenLocal()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
-    compileOnly("org.spigotmc:spigot:1.19.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
     compileOnly("org.xerial:sqlite-jdbc:3.43.2.2")
 }
 
-tasks{
-    processResources{
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+tasks {
+    processResources {
         from(sourceSets.main.get().resources.srcDirs) {
             filesMatching("plugin.yml") {
                 expand(
